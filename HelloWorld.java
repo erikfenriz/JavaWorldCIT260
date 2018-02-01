d92f40c4a43f448d6cb02ea4de902eae44b0cade
RichardsonRayshorn <rayshorndc@gmail.com>
2018-02-01 16:00:15 -0500

    Hello World Java
    Test Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/***************************
+ *Run this to say hello world
+ *@author Rayshorn Richardson
+ **************************/
+
+class HelloWorld{
+    
+    public static void main(String[] arg){
+        
+        System.out.println("Hello World");
+    }
+}
