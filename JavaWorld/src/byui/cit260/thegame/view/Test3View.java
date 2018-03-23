0fc89e862e2b459dd3a712f1a27878d899a4e3e2
Erik Rybalkin <erikfenriz@gmail.com>
2018-03-23 21:57:16 +0200

    fix

--- JavaWorld/src/byui/cit260/thegame/view/Test3View.java
+++ JavaWorld/src/byui/cit260/thegame/view/Test3View.java
@@ -9,6 +9,25 @@ package byui.cit260.thegame.view;
  *
  * @author user
  */
-public class Test3View {
+public class Test3View extends View{
+
+       public double ParseDouble(){
+        
+        String anotherValue = "value"; 
+        try {
+    return Double.parseDouble(anotherValue);
+} catch(NumberFormatException e) {
+    e.printStackTrace(); //do something
+       return -1;
+   }
+        
+    
+        
+    }
+    
+    @Override
+    public boolean doAction(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
