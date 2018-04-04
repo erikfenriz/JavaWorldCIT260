d8f19419a87393d3cf0dcb6b192526b22aa0c328
RichardsonRayshorn <rayshorndc@gmail.com>
2018-04-04 12:03:23 -0500

    Items control

--- JavaWorld/src/byui/cit260/thegame/control/Items.java
+++ JavaWorld/src/byui/cit260/thegame/control/Items.java
@@ -5,10 +5,53 @@
  */
 package byui.cit260.thegame.control;
 
+import byui.cit260.thegame.model.Item;
+import byui.cit260.thegame.model.KnowledgeCoin;
+import byui.cit260.thegame.model.Shop;
+
 /**
  *
- * @author user
+ * @author Rayshorn Richardson
  */
-class Items {
+public class Items {
+    
+    public KnowledgeCoin currentCurrency;
+    public Item item;
+    public Shop shop;
+    public static double IncreaseCurrency(Item item,Shop shop, KnowledgeCoin currentCurrency) {
+        if(shop.isBuy() == false && shop.isSell() == false){
+            return 0;
+        }
+        else if(shop.isBuy() == true && shop.isSell() == false){
+            
+        }
+
+
+        
+        return 1;
+    }    
     
 }
+
+ /*calcIncreaseHunger(Item, Item cost, buy, Sell, currentCurrency): Int
+BEGIN
+IF (buy && sell == false)
+RETURN “This item can’t increase currency”
+ELSE IF (buy == true && sell == false)
+	Inventory inventory =  new Inventory;
+	Inventory.addItem = item;
+	currentCurrency = currentCurrency – itemCost;
+	RETURN currentCurrency;
+ELSE IF (buy == false && sell == true)
+ 	Inventory inventory = new Inventory;
+Inventory.removeItem = item;
+currentCurrency = currentCurrency + itemCost;
+ 	RETURN currentCurrency;
+ELSE IF (buy == true && sell == false && currentCurrency <= 0) 
+ 	RETURN “You don’t have any money to buy this item”
+ELSE IF(buy == false && sell == true && itemCost <= 0)
+ 	Inventory inventory = new Inventory;
+Inventory.removeItem = item;
+RETURN “Item isn’t worth any currency”
+ELSE
+RETURN bug*/
